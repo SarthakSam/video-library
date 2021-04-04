1be3448a614fd63ae2dd6fb0031837f0460041ec
Sarthak <sarthak242@gmail.com>
2021-04-04 17:41:09 +0530

    Style: styled navbar

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,13 +1,23 @@
 import styles from './Nav.module.css';
-import { FaBars } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
-export function Nav({ setSideNavState }) {
+export function Nav(x) {
 
 
     return (
-        <nav>
-            I am nav
-
+        <nav className = "nav">
+            <span className="nav__title">Streamit</span>
+            <div className="nav__content">
+                <ul className={"nav__list " + styles.search__input__container}>
+                    <li className={"input input--icon " + styles.search__input}>
+                        <input type="search" placeholder="search"/>
+                        <FaSearch />
+                    </li>
+                </ul>
+                <ul className="nav__list">
+                    <li className="nav__item">Sarthak</li>                
+                </ul>
+            </div>
         </nav>
     )
 }
