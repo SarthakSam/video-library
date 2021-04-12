be81c8e3cce7d6f1668eeccfa722c86c2f01de67
Sarthak <sarthak242@gmail.com>
2021-04-12 12:59:49 +0530

    Feat: added video apis

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,31 @@
-const express = require('express'),
-      app     = express();
+const express    = require('express'),
+      app        = express(),
+      bodyParser = require('body-parser'),
+      videos     = require('./mock-data/videos');
 
 const PORT = 3001;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send("Streamit Server");
+});
+
+app.get('/videos', (req, res) => {
+    res.json({ videos, message: "Success" });
+});
+
+app.post('/videos', (req, res) => {
+    const video = req.body;
+    console.log(video);
+    videos.push(video);
+    res.json(201).json({ message: "Success" });
+}); 
+
+app.get('/videos/:id', (req, res) => {
+    const { id } = req.params;
+    const video = videos.find(video => video.id === id);
+    video? res.json( { message: 'Success', video } ) : res.status(404).json( { message: 'No video found with this id'} )
 });
 
 app.listen(PORT, (err) => {
